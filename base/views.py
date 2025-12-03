0285db7f98f784bd77b871795e5cad38d1c29b9f
ivantea <ivanka784522@gmail.com>
2025-12-03 18:01:07 +0300

    modified:   base/__pycache__/views.cpython-313.pyc 	modified:   base/templates/base/home.html 	new file:   base/templates/base/login_register.html 	modified:   base/views.py 	modified:   templates/navbar.html

--- base/views.py
+++ base/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
-from .models import Room, Topic 
+from .models import Room, Topic
+from django.db.models import Q 
 from .form import RoomForm
+from django.contrib.auth import authenticate, login 
+from django.contrib import messages
 
 
 def home (request): 
 
-    rooms = Room.objects.all()
+    
+    q = request.GET.get('q') if request.GET.get('q') != None else ' '
+
+    rooms = Room.objects.filter(Q(topic__name__icontains = q) |
+                                Q(host__username__icontains = q) )
     topics = Topic.objects.all()
 
     context = {'rooms' : rooms, 'topics' : topics}
@@ -49,3 +56,19 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect ('home')
     return render (request, 'base/delete.html', {'obj': room})
+
+def loginPage(request): 
+    if request.method == 'POST': 
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try: 
+            user = User.objects.get(username = username)
+        except: 
+            messages.error(request, 'User does not exist')
+        user = authenticate(request, username = username, password = password)
+        if user is not None: 
+            login(request, user)
+            return redirect ('home') 
+    context = {}
+    return render (request, 'base/login_register.html', context)            
